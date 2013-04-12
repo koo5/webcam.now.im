648199b6beb34eb00d3024cc7b3af33429c7e3e5
webcam <webcam@webcam.(none)>
2013-04-12 08:21:21 +0200

    faster harder better suck it meway

--- runloop.sh
+++ runloop.sh
@@ -1,10 +1,10 @@
 while true;
     scrot cam.png -t 20
-    montage -geometry +0+0 -background white -label "`date -u`" -quality 95 -depth 8 cam.png cam2.png
-
-    mv cam2.png /var/www/cam.png;
+#    montage -geometry +0+0 -background white -label "`date -u`" -quality 95 -depth 8 cam.png cam2.png
+    date -u > /var/www/timestamp
+    mv cam.png /var/www/cam.png;
     mv cam-thumb.png /var/www/cam-thumb.png;
 
-    do sleep 5;
+    do sleep 2;
     
     done
--- runvnc.sh
+++ runvnc.sh
@@ -2,3 +2,4 @@
 
 vncserver -localhost -rfbauth .vnc/passwd -nolisten tcp -geometry 640x480
 
+declare -x DISPLAY=:1
