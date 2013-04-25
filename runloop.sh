759b3afda77aa0e722b271e78b74813dcbe26db8
webcam <webcam@webcam.(none)>
2013-04-25 04:14:21 +0200

    budding complete

--- runloop.sh
+++ runloop.sh
@@ -1,12 +1,23 @@
 cd /tmp/webcam
-export DISPLAY=:1
-while true;
-    scrot cam0.png -t 20
-#    montage -geometry +0+0 -background white -label "`date -u`" -quality 95 -depth 8 cam.png cam2.png
-    date -u > timestamp
-    mv cam0.png cam.png;
-    mv cam0-thumb.png cam-thumb.png;
+while true; do
 
-    do sleep 2;
-    
+    export DISPLAY=:0
+    scrot camtemp.png -t 40
+    mv camtemp.png cam0.png;
+    mv camtemp-thumb.png cam0-thumb.png;
+    export DISPLAY=:1
+    scrot camtemp.png -t 40
+    mv camtemp.png cam1.png;
+    mv camtemp-thumb.png cam1-thumb.png;
+    export DISPLAY=:2
+    scrot camtemp.png -t 40
+    mv camtemp.png cam2.png;
+    mv camtemp-thumb.png cam2-thumb.png;
+
+    date -u > timestamp
+    sleep 2;
     done
+
+
+
+#    montage -geometry +0+0 -background white -label "`date -u`" -quality 95 -depth 8 cam.png cam2.png
--- runvnc.sh
+++ runvnc.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+cd /home/webcam
+vncserver :0 -localhost -rfbauth .vnc/passwd -nolisten tcp -geometry 640x480 &
+vncserver :1 -localhost -rfbauth .vnc/passwd -nolisten tcp -geometry 640x480 &
+vncserver :2 -localhost -rfbauth .vnc/passwd -nolisten tcp -geometry 640x480 &
 
-vncserver -localhost -rfbauth .vnc/passwd -nolisten tcp -geometry 640x480
+sleep 6
 
-declare -x DISPLAY=:1
+
+/home/webcam/runmana.sh&
+
+#/home/webcam/runloop.sh
+
 
